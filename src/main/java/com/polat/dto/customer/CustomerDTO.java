58ccfbbcb2b8c002afd1c7c71d879f2a07557f89
Polat <mustfplt4@gmail.com>
2023-05-22 14:04:00 +0300

    customerDTO added

---
+++ src/main/java/com/polat/dto/customer/CustomerDTO.java
@@ -0,0 +1,14 @@
+package com.polat.dto.customer;
+
+import com.polat.model.Invoice;
+
+import java.util.List;
+
+/**
+ * @author MPolat
+ */
+public record CustomerDTO(Long id,
+                          String name,
+                          String surname,
+                          List<Invoice> invoiceList) {
+}
---
+++ src/main/java/com/polat/dto/customer/CustomerSaveRequest.java
@@ -0,0 +1,8 @@
+package com.polat.dto.customer;
+
+/**
+ * @author MPolat
+ */
+public record CustomerSaveRequest(String name,
+                                  String surname) {
+}
