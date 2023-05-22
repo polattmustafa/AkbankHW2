ed6c77ffcc758294ec3e5c8dec951ef21a9e5112
Polat <mustfplt4@gmail.com>
2023-05-22 13:18:50 +0300

    added src

---
+++ src/main/java/com/polat/AkbankHw2Application.java
@@ -0,0 +1,13 @@
+package com.polat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbankHw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkbankHw2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/polat/AkbankHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkbankHw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
