73575dba1541433ab69680ba9808eafe574831de
Polat <mustfplt4@gmail.com>
2023-05-22 22:03:26 +0300

    added constans

---
+++ src/main/java/com/polat/general/Constants.java
@@ -0,0 +1,10 @@
+package com.polat.general;
+
+/**
+ * @author MPolat
+ */
+public final class Constants {
+    public static final double INVOICE_AMOUNT_THRESHOLD = 1500.0;
+    public static final double INVOICE_AMOUNT_BELOW = 500.0;
+
+}
